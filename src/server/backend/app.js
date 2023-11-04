02124607f208d92246de696335471a8bd3614de1
WillMW02 <williammw.wmw@gmail.com>
2023-11-04 21:24:34 +0000

    feat: serve react page from express

--- src/server/backend/app.js
+++ src/server/backend/app.js
@@ -3,6 +3,8 @@ import cors from 'cors';
 import helmet from 'helmet';
 import compression from 'compression';
 import bodyParser from 'body-parser';
+import { join, dirname} from 'path';
+import { fileURLToPath } from 'url';
 import cookieParser from 'cookie-parser';
 
 
@@ -12,6 +14,7 @@ import LoggerMiddleware from './middlewares/logger.middleware.js';
 import MethodMiddleware from './middlewares/methods.middleware.js';
 
 const app = express();
+const __dirname = fileURLToPath(dirname(import.meta.url));
 
 app.use(express.json());
 app.use(bodyParser.json());
@@ -25,6 +28,7 @@ app.use(cors());
 app.use(LoggerMiddleware);
 app.use(MethodMiddleware);
 
+app.use(express.static(join(__dirname, '../../client/public')));
 app.use('/api/', ApiRouter);
 
 

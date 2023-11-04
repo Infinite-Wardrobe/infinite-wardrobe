0b421f3539caba92192ed421dfdbf1e48f8729e9
WillMW02 <williammw.wmw@gmail.com>
2023-11-04 21:25:17 +0000

    feat: add basic controllers and auth
    Add clothing controller and routes, implement auth
    middleware

---
+++ src/server/backend/controllers/clothing.controller.js
@@ -0,0 +1,26 @@
+import { ClothingModel } from "../schemas/clothing.schema.js";
+
+// GET /
+export const getClothing = async (req, res) => {
+	return res.status(501).json({ message: 'Not implemented' });
+};
+
+// GET /:id
+export const getItem = async (req, res) => {
+	return res.status(501).json({ message: 'Not implemented' });
+};
+
+// POST /
+export const createItem = async (req, res) => {
+	return res.status(501).json({ message: 'Not implemented' });
+};
+
+// PUT /:id
+export const updateItem = async (req, res) => {
+	return res.status(501).json({ message: 'Not implemented' });
+};
+
+// DELETE /:id
+export const deleteItem = async (req, res) => {
+	return res.status(501).json({ message: 'Not implemented' });
+};
--- src/server/backend/routes/api.route.js
+++ src/server/backend/routes/api.route.js
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 
 import AuthRouter from './auth.route.js';
+import ClothingRouter from './clothing.route.js';
 
 const router = Router();
 
-router.use('auth', AuthRouter);
+router.use('/auth', AuthRouter);
+router.use('/clothing', ClothingRouter);
 
 router.get('/teapot', (req, res, next) => res.status(418).json({ message: 'I\'m a teapot' }));
 
-export default Router;
+export default router;

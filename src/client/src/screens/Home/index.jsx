c372bc3ac93de753ec100ca020700f7c72c9a81f
Yosef Berezovskiy <87656348+russianambassador@users.noreply.github.com>
2023-11-05 12:03:27 +0000

    finishing

--- src/client/src/screens/Home/index.jsx
+++ src/client/src/screens/Home/index.jsx
@@ -1,10 +1,15 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import * as Components from "../../components";
+import CategoryFilter from "../../components/CategoryFilter/CategoryFilter";
 import styles from "./styles.module.css";
 
 function Home() {
   const [data, setData] = useState({ info: "", clothing: [] });
+  const [selectedColor, setSelectedColor] = useState("");
+  const [selectedClothingType, setSelectedClothingType] = useState("");
+  const [colors, setColors] = useState([]);
+  const [clothingTypes, setClothingTypes] = useState([]);
 
   useEffect(() => {
     // Fetch users from the API
@@ -13,6 +18,16 @@ function Home() {
       .then((response) => {
         if (response.data && Array.isArray(response.data.clothing)) {
           setData(response.data);
+          const unqiueColors = [
+            ...new Set(response.data.clothing.map((item) => item.colour)),
+          ];
+          const unqiueTypes = [
+            ...new Set(response.data.clothing.map((item) => item.category)),
+          ];
+          setColors(unqiueColors);
+          setClothingTypes(unqiueTypes);
+          // console.log(unqiueColors);
+          // console.log(unqiueTypes);
         } else {
           console.error("API did not return the expected structure");
         }
@@ -21,24 +36,37 @@ function Home() {
         console.error("Error fetching users:", error);
       });
   }, []);
-  console.log(data.clothing);
+  // console.log(data.clothing);
+
   return (
     <div>
-      <p>{data.info}</p>
-      <ul>
-        {(() => {
-          const items = [];
-          data.clothing.forEach((item) => {
-            items.push(
-              <Components.Card
-                imgUrl={item.imageString}
-                title={item.category}
-              />
-            );
-          });
-          return items;
-        })()}
-      </ul>
+      <CategoryFilter
+        selectedColor={selectedColor}
+        setSelectedColor={setSelectedColor}
+        selectedClothingType={selectedClothingType}
+        setSelectedClothingType={setSelectedClothingType}
+        clothingTypes={clothingTypes}
+        colors={colors}
+      />
+      {(() => {
+        const items = [];
+        data.clothing.forEach((item) => {
+          if (item.colour == selectedColor || selectedColor == "") {
+            if (
+              item.category == selectedClothingType ||
+              selectedClothingType == ""
+            ) {
+              items.push(
+                <Components.Card
+                  imgUrl={item.imageString}
+                  title={item.category}
+                />
+              );
+            }
+          }
+        });
+        return items;
+      })()}
     </div>
   );
 }
